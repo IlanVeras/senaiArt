c284dc4d7d34af8a8542ff50b616aad8aa916f22
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-04-16 22:55:20 -0400

    icons footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
     <div className={`${styles.container} container-fluid`}>
       <p>Nos encontre nas redes sociais</p>
       <div className={`${styles.socials}`}>
-        <img src="../src/assets/facebook.png" alt=""/>
-        <img src="../src/assets/instagram.png" alt="" />
-        <img src="../src/assets/twitter.png" alt="" />
+        <img src="/assets/facebook.png" alt=""/>
+        <img src="/assets/instagram.png" alt="" />
+        <img src="/assets/twitter.png" alt="" />
       </div>
       <p>Art 2025</p>
     </div>
